b0f158b78805d75d3991e04264ca8547b462c3cb
Вадим Шаларь <78111178+Achimenes-freeman@users.noreply.github.com>
2022-04-06 22:58:54 +0300

    Add files via upload

---
+++ src/components/emp-list/emp-list.js
@@ -0,0 +1,15 @@
+import EmpListItem from '../emp-list-item/emp-list-item';
+
+import './emp-list.css';
+
+const EmpList = () =>{
+    return(
+        <ul className="app-list list-group">
+            <EmpListItem/>
+            <EmpListItem/>
+            <EmpListItem/>
+        </ul>
+    );
+}
+
+export default EmpList;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css';
+
+const SearchPanel = () =>{
+    return(
+        <input 
+            type="text"
+            className="form-control search-input"
+            placeholder="Нати сотрудника" />
+    );
+}
+
+export default SearchPanel;
